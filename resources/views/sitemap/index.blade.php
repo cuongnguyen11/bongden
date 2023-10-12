3d235f74178251c3f55f05db482fbee5026b985f
Cuongdao2222 <cuongdao12121@gmail.com>
2023-10-12 08:21:04 +0700

    update

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,24 +1,14 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    @if(isset($arr_number))
-    @foreach($arr_number as $numbers)
+    @if(isset($products))
+    @foreach($products as $value)
         <sitemap>
-            <loc>https://dienmaynguoiviet.vn/sitemap_pc{{trim($numbers)}}.xml</loc>
+            <loc>{{ route('details', $value->Links)  }}</loc>
             <lastmod>{{ Carbon\Carbon::now()->format('Y-m-d') }}</lastmod>
         </sitemap>
     @endforeach    
     @endif
-    <sitemap>
-        <loc>https://dienmaynguoiviet.vn/sitemap_brand.xml</loc>
-        <lastmod>{{ Carbon\Carbon::now()->format('Y-m-d') }}</lastmod>
-    </sitemap>
-    <sitemap>
-        <loc>https://dienmaynguoiviet.vn/sitemap_pc.xml</loc>
-        <lastmod>{{ Carbon\Carbon::now()->format('Y-m-d') }}</lastmod>
-    </sitemap>
-    <sitemap>
-        <loc>https://dienmaynguoiviet.vn/sitemap_article.xml</loc>
-        <lastmod>{{ Carbon\Carbon::now()->format('Y-m-d') }}</lastmod>
-    </sitemap>
+    
+   
 </sitemapindex>
