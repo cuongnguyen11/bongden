5c0bc51c46cf8d5aae062b736475035e52dd7f4c
Cuongdao2222 <cuongdao12121@gmail.com>
2023-11-07 13:44:50 +0700

    Update details.blade.php

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -132,7 +132,7 @@
                                             <path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" class=""></path>
                                         </svg> </span> <span class="star-on star"> <svg aria-hidden="true" data-prefix="fas" data-icon="star" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-star fa-w-18">
                                             <path fill="currentColor" d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" class=""></path>
-                                        </svg> </span> <span itemprop="ratingValue" class="hide">5</span> <span itemprop="bestRating" class="hide">5</span> <a href="#prodetails_tab3" title="Đánh giá sản phẩm này" class=""> <span itemprop="ratingCount">{{  $count_comment }}</span> đánh giá / 5 lượt mua </a> </span> </div>
+                                        </svg> </span> <span itemprop="ratingValue" class="hide">5</span> <span itemprop="bestRating" class="hide">5</span> <a href="#prodetails_tab3" title="Đánh giá sản phẩm này" class=""> <span itemprop="ratingCount">1</span> đánh giá / 5 lượt mua </a> </span> </div>
                         </div>
                         
                     </div>
